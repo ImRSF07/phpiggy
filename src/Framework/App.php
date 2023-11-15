e3ef8b130156c292e31458b01c590d544a163a88
ImRSF07 <iamrsf0795@gmail.com>
2023-11-15 10:15:34 +0800

    Completion of section 12

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -6,8 +6,34 @@ namespace Framework;
 
 class App
 {
+    private Router $router;
+    private Container $container;
+
+    public function __construct(string $containerDefinitionsPath = null)
+    {
+        $this->router = new Router();
+        $this->container = new Container();
+
+        if ($containerDefinitionsPath) {
+            $containerDefinitionsPath = include $containerDefinitionsPath;
+            $this->container->addDefinitions($containerDefinitionsPath);
+        }
+    }
     public function run()
     {
-        echo "Application running";
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $this->router->dispatch($path, $method, $this->container);
+    }
+
+    public function get(string $path, array $controller)
+    {
+        $this->router->add('GET', $path, $controller);
+    }
+
+    public function addMiddleware(string $middleware)
+    {
+        $this->router->addMiddleware($middleware);
     }
 }
